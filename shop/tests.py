5001201d7412ec326ea135d8b75680e167cc25ba
log-Z <chenyongle38@gmail.com>
2019-04-09 19:41:11 +0800

    修改用户和商品模型的测试用例

--- shop/tests.py
+++ shop/tests.py
@@ -3,11 +3,16 @@ from django.test import TestCase
 from .models import User, Goods
 
 
+class UserModelTest(TestCase):
+
+    def createUserTest(self):
+        u = User.objects.create(username='abc', password='123', email='a@qq.com')
+        self.assertIn(u, User.objects.all())
+
+
 class GoodsModelTest(TestCase):
 
     def createGoodsTest(self):
         u = User.objects.create(username='abc', password='123', email='a@qq.com')
-        self.assertIn(u, User.objects.all())
-
         g = Goods.objects.create(goods_name='pc', seller=u, price=5999.99)
         self.assertIn(g, Goods.objects.all())
