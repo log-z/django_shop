6e5fcdb7eb6fa267fa2f6cfae7f11e8d4461f507
log-Z <chenyongle38@gmail.com>
2019-04-09 22:27:46 +0800

    开始实现商品列表页面

--- shop/admin.py
+++ shop/admin.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 
 from .models import User, Goods
 
+
 admin.site.register(User)
 admin.site.register(Goods)
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
+from django.views import  generic
+
+from .models import Goods
 
 
 def index(request):
     return HttpResponse('shop index page.')
+
+
+class IndexView(generic.ListView):
+    model = Goods
+    template_name = 'shop/index.html'
