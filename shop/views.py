4abd4777a6451f229a058dfafcc5a24907721a9d
log-Z <chenyongle38@gmail.com>
2019-04-12 23:58:44 +0800

    添加商品检索功能接口，先实现按商户检索

--- shop/views.py
+++ shop/views.py
@@ -6,8 +6,20 @@ from .models import Goods
 class GoodsListView(generic.ListView):
     """商品列表视图"""
 
-    model = Goods
     template_name = 'shop/goods_list.html'
+
+    def get_queryset(self):
+        queryset = Goods.objects.all()
+
+        if 'g' in self.request.GET:
+            goods_kws = self.request.GET['g']
+            queryset = queryset.filter(goods_name__contains=goods_kws)
+
+        if 's' in self.request.GET:
+            seller_id = self.request.GET['s']
+            queryset = queryset.filter(seller_id=seller_id)
+
+        return queryset
 
 
 class GoodsDetailView(generic.DetailView):
