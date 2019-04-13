046d3f4252bac5477dded6b478b718f70cfc53c4
log-Z <chenyongle38@gmail.com>
2019-04-13 19:37:45 +0800

    添加商品列表测试，修复商品图片缺省导致页面出错的问题

--- shop/tests.py
+++ shop/tests.py
@@ -6,7 +6,7 @@ from .models import User, Goods
 class UserModelTest(TestCase):
     """用户模型测试"""
 
-    def createUserTest(self):
+    def test_create_user(self):
         u = User.objects.create(username='abc', password='123', email='a@qq.com')
         self.assertIn(u, User.objects.all())
 
@@ -14,10 +14,33 @@ class UserModelTest(TestCase):
 class GoodsModelTest(TestCase):
     """商品模型测试"""
 
-    def createGoodsTest(self):
+    def test_create_goods(self):
         u = User.objects.create(username='abc', password='123', email='a@qq.com')
         g = Goods.objects.create(goods_name='pc', seller=u, price=5999.99)
         self.assertIn(g, Goods.objects.all())
 
-# class GoodsListViewTest(TestCase):
-    # TODO: 未完成商品检索测试
+
+class GoodsListViewTest(TestCase):
+    """商品列表视图测试"""
+
+    def test_base_object_show(self):
+        u = User.objects.create(username='abc', password='123', email='a@qq.com')
+        g = Goods.objects.create(goods_name='pc', seller=u, price=5999.99)
+
+        response = self.client.get('/shop/')
+        self.assertContains(response, g.goods_name)
+        self.assertContains(response, g.seller)
+        self.assertContains(response, g.price)
+        self.assertContains(response, 'default_goods_image.png')
+
+    def test_full_object_show(self):
+        u = User.objects.create(username='abc', password='123', email='a@qq.com')
+        g = Goods.objects.create(goods_name='pc', seller=u, price=9.9, image='image.png', description='一些奇奇怪怪的描述')
+
+        response = self.client.get('/shop/')
+        self.assertNotContains(response, g.description)
+        self.assertNotContains(response, 'default_goods_image.png')
+        self.assertContains(response, 'image.png')
+
+
+# TODO: 未完成商品检索测试
